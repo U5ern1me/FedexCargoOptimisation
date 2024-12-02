cb69d7e75560e5afa6ed6061d7bf009f25cac2b5
Parth Patil <psp90804@gmail.com>
2024-12-03 03:53:22 +0530

    fix requirements.txt

--- src/strategies/hexaly_strategy/hexaly_strategy.py
+++ src/strategies/hexaly_strategy/hexaly_strategy.py
@@ -1,14 +1,16 @@
 import os
 import sys
-from utils.io import load_config
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from strategies.strategy import Strategy
-from .hexaly_utils import *
-import pandas as pd
+import hexaly.optimizer
 
-config = load_config(os.path.join(os.path.dirname(__file__),"hexaly.config"))
+from strategies.strategy import Strategy
+from .hexaly_utils import HexalySolver
+from utils.io import load_config
+
+config = load_config(os.path.join(os.path.dirname(__file__), "hexaly.config"))
+
 
 class HexalyStrategy(Strategy):
     def solve(self):
@@ -16,24 +18,33 @@ class HexalyStrategy(Strategy):
             model = optimizer.model
             try:
                 optimizer.param.time_limit = config["total_timesteps"]
-                solver = Solver(self.packages, self.ulds, self.k_cost, model=model)
-            
+                solver = HexalySolver(
+                    self.packages, self.ulds, self.k_cost, model=model
+                )
+
                 model.close()
 
                 optimizer.solve()
 
+                # Assign ULD ID to each package
                 for i in range(len(self.packages)):
                     for j in range(len(self.ulds)):
                         if i in solver.container_content[j]:
-                            self.packages[i].uld_id = self.ulds[i].id
+                            self.packages[i].uld_id = self.ulds[j].id
+                            break
 
+                # Assign package coordinates
                 for i in range(len(self.packages)):
-                    self.packages[i].point1 = (solver.x[i].value, solver.y[i].value, solver.z[i].value)
-                    self.packages[i].point2 = (solver.x[i].value+solver.used_l[i].value, solver.y[i].value+solver.used_w[i].value, solver.z[i].value+solver.used_h[i].value)
- 
+                    self.packages[i].point1 = (
+                        solver.x[i].value,
+                        solver.y[i].value,
+                        solver.z[i].value,
+                    )
+                    self.packages[i].point2 = (
+                        solver.x[i].value + solver.used_l[i].value,
+                        solver.y[i].value + solver.used_w[i].value,
+                        solver.z[i].value + solver.used_h[i].value,
+                    )
+
             except Exception as e:
-                print("Packing failed")
-                print(e)
-
-
-
+                print(f"Packing failed: {e}")
