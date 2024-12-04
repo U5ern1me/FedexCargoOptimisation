0cb4b648f1540681e9f004214dfb3898b2b7247a
Parth Patil <psp90804@gmail.com>
2024-12-05 02:36:55 +0530

    heuristic selection of heuristics

--- src/strategies/greedy_heuristic_strategy/greedy_heuristic_utils.py
+++ src/strategies/greedy_heuristic_strategy/greedy_heuristic_utils.py
@@ -1,17 +1,11 @@
 from itertools import combinations
 import logging
-import copy
 import os
-
-from utils.io import load_config
-from solvers import solvers
 
 # typing
 from typing import List, Callable, Tuple
 from models.uld import ULD
 from models.package import Package
-
-config = load_config(os.path.join(os.path.dirname(__file__), "greedy_heuristic.config"))
 
 
 def get_all_division_of_ulds(ulds: List[ULD], check_top: int) -> List[List[List[ULD]]]:
@@ -126,9 +120,10 @@ def get_divide_into_priority_and_economic(
     return priority_packages, economic_packages
 
 
-def sorting_heuristic_1(package: Package) -> float:
+def base_sorting_heuristic_1(package: Package) -> float:
     """
     Heuristic to sort the packages.
+    Priority is given to volume.
 
     Args:
         package: Package
@@ -140,10 +135,10 @@ def sorting_heuristic_1(package: Package) -> float:
     return package.delay_cost / volume
 
 
-def sorting_heuristic_2(package: Package) -> float:
+def base_sorting_heuristic_2(package: Package) -> float:
     """
     Heuristic to sort the packages.
-
+    Priority is given to weight.
     Args:
         package: Package
 
@@ -153,9 +148,10 @@ def sorting_heuristic_2(package: Package) -> float:
     return package.delay_cost / (package.weight)
 
 
-def sorting_heuristic_3(package: Package) -> float:
+def base_sorting_heuristic_3(package: Package) -> float:
     """
     Heuristic to sort the packages.
+    Priority majorly to volume with a small weight component.
 
     Args:
         package: Package
@@ -166,6 +162,52 @@ def sorting_heuristic_3(package: Package) -> float:
     return package.delay_cost / (
         package.length * package.width * package.height * (package.weight**0.15)
     )
+
+
+def base_sorting_heuristic_4(package: Package) -> float:
+    """
+    Heuristic to sort the packages.
+    Priority majorly to weight with a small volume component.
+
+    Args:
+        package: Package
+
+    Returns:
+        Value to sort the packages
+    """
+    return package.delay_cost / (
+        package.weight * (package.length * package.width * package.height) ** 0.15
+    )
+
+
+def get_sorting_heuristics(
+    average_package_density: float, average_uld_density: float
+) -> Tuple[Callable, Callable]:
+    """
+    Get the sorting heuristics based on the average package and uld densities.
+
+    Args:
+        average_package_density: Average package density
+        average_uld_density: Average ULD density
+
+    Returns:
+        sorting_heuristic_1, sorting_heuristic_2
+    """
+    ratio = average_package_density / average_uld_density
+    if ratio < 1.3:
+        # Give more priority to volume
+        sorting_heuristic_1 = base_sorting_heuristic_1
+        sorting_heuristic_2 = base_sorting_heuristic_3
+    elif ratio < 2.1:
+        # Give mixed priority to volume and weight
+        sorting_heuristic_1 = base_sorting_heuristic_3
+        sorting_heuristic_2 = base_sorting_heuristic_4
+    else:
+        # Give more priority to weight
+        sorting_heuristic_1 = base_sorting_heuristic_2
+        sorting_heuristic_2 = base_sorting_heuristic_4
+
+    return sorting_heuristic_1, sorting_heuristic_2
 
 
 def virtual_fit_priority(
@@ -295,211 +337,3 @@ async def find_splits_economic_packages(
         logging.info(f"Found split 2: {split_2}")
 
     return (split_1, split_2)
-
-
-def calculate_cost(packages: List[Package], k_cost: float) -> float:
-    priority_ulds = set()
-    delay_cost = 0
-    for package in packages:
-        if package.uld_id is None:
-            delay_cost += package.delay_cost
-        else:
-            if package.priority:
-                priority_ulds.add(package.uld_id)
-
-    return delay_cost + k_cost * len(priority_ulds)
-
-
-async def main_solving_wrapper(
-    packages: List[Package],
-    ulds: List[ULD],
-    k_cost: float,
-    sorting_heuristic_1: Callable = sorting_heuristic_1,
-    sorting_heuristic_2: Callable = sorting_heuristic_2,
-    verbose: bool = False,
-):
-    packages = copy.deepcopy(packages)
-    ulds = copy.deepcopy(ulds)
-
-    # initialize solver
-    solver = solvers[config["solver"]]
-
-    if verbose:
-        logging.info(f"using solver: {solver.__name__}")
-    # divide packages into priority and economic
-    priority_packages, economic_packages = get_divide_into_priority_and_economic(
-        packages,
-    )
-
-    if verbose:
-        logging.info(f"Priority packages: {len(priority_packages)}")
-        logging.info(f"Economic packages: {len(economic_packages)}")
-        logging.info(f"Sorting heuristic 1: {sorting_heuristic_1.__name__}")
-        logging.info(f"Sorting heuristic 2: {sorting_heuristic_2.__name__}")
-
-    # sort economic packages by delay cost per kg
-    sorted_economic_packages = sort_packages(economic_packages, sorting_heuristic_1)
-
-    # get all possible splits of ulds
-    uld_splits_arr = get_all_division_of_ulds(ulds, 1)
-
-    if verbose:
-        logging.info(f"{uld_splits_arr}")
-
-    # initialize best split value and packages
-    best_split_value = k_cost * (len(ulds) + 1)
-    best_split_packages = (0, 0)
-    best_split_uld_splits = (None, None)
-
-    for num_p_uld, uld_splits in enumerate(uld_splits_arr):
-
-        if verbose:
-            logging.info(f"Checking {num_p_uld} ULDs")
-
-        # check if priority packages fit in uld splits if not then it is not a valid split
-        valid_uld_splits = []
-        for uld_split in uld_splits:
-            uld_group_1, uld_group_2 = split_ulds_into_two(ulds, uld_split)
-            if virtual_fit_priority(priority_packages, uld_group_1):
-                valid_uld_splits.append((uld_group_1, uld_group_2))
-
-        # if no valid splits then continue
-        if len(valid_uld_splits) == 0:
-            continue
-
-        if verbose:
-            logging.info(f"Found {len(valid_uld_splits)} valid ULD splits")
-
-        local_best_split_packages = (0, 0)
-        local_best_split_uld_splits = (None, None)
-
-        # find the best split of economic packages for each valid uld split
-        for uld_group_1, uld_group_2 in valid_uld_splits:
-            splits = await find_splits_economic_packages(
-                sorted_economic_packages,
-                priority_packages,
-                uld_group_1,
-                uld_group_2,
-                solver,
-                verbose=verbose,
-            )
-
-            # if no splits then continue (priority cannot fit in uld group 1)
-            if splits is None:
-                if verbose:
-                    logging.info("Could not fit the priority packages in ULD group 1")
-                continue
-
-                # for a split with more economic packages, update the best split (number of ulds in both uld splits must be same for this)
-            if sum(splits) > sum(local_best_split_packages):
-                local_best_split_packages = splits
-                local_best_split_uld_splits = (uld_group_1, uld_group_2)
-
-            if verbose:
-                logging.info(f"Found local best split: {local_best_split_packages}")
-
-            # calculate the cost of the local best split
-            delay_cost = sum(
-                [
-                    package.delay_cost
-                    for package in sorted_economic_packages[
-                        local_best_split_packages[0] + local_best_split_packages[1] :
-                    ]
-                ]
-            )
-            spread_cost = k_cost * num_p_uld
-            total_cost = delay_cost + spread_cost
-
-            # if the cost of the local best split is less than the best split value, update the best split
-            if total_cost < best_split_value:
-                best_split_value = total_cost
-                best_split_packages = local_best_split_packages
-                best_split_uld_splits = local_best_split_uld_splits
-
-        if verbose:
-            logging.info(f"Current best split: {best_split_value}")
-
-    # get the final partition of packages
-    partition_1 = [
-        *priority_packages,
-        *sorted_economic_packages[: best_split_packages[0]],
-    ]
-    partition_2 = sorted_economic_packages[
-        best_split_packages[0] : best_split_packages[0] + best_split_packages[1]
-    ]
-
-    remaining_packages = sorted_economic_packages[
-        best_split_packages[0] + best_split_packages[1] :
-    ]
-
-    # get the remaining packages
-    remaining_packages = sort_packages(remaining_packages, sorting_heuristic_2)
-
-    if verbose:
-        logging.info(f"Remaining packages: {len(remaining_packages)}")
-
-    _remaining_packages = []
-    for package_num, package in enumerate(remaining_packages[: config["error tuning"]]):
-        if verbose:
-            logging.info(
-                f"Checking package {package_num} of {config['error tuning']} packages"
-            )
-
-        if best_split_uld_splits[0] is not None and len(best_split_uld_splits[0]) > 0:
-            _partition_1 = partition_1 + [package]
-            _solver_1 = solver(
-                ulds=best_split_uld_splits[0],
-                packages=_partition_1,
-            )
-            await _solver_1.solve(only_check_fits=True)
-            if await _solver_1.get_fit():
-                partition_1.append(package)
-                continue
-
-        if best_split_uld_splits[1] is not None and len(best_split_uld_splits[1]) > 0:
-            _partition_2 = partition_2 + [package]
-            _solver_2 = solver(
-                ulds=best_split_uld_splits[1],
-                packages=_partition_2,
-            )
-            await _solver_2.solve(only_check_fits=True)
-            if await _solver_2.get_fit():
-                partition_2.append(package)
-                continue
-
-        _remaining_packages.append(package)
-
-    remaining_packages = (
-        _remaining_packages + remaining_packages[config["error tuning"] :]
-    )
-
-    if verbose:
-        logging.info(f"Remaining packages: {len(remaining_packages)}")
-
-    # solve for both partitions and assign the solution to the strategy (done in the solver)
-    # check if the partition and uld split is valid
-    if (
-        best_split_uld_splits[0] is not None
-        and len(partition_1) > 0
-        and len(best_split_uld_splits[0]) > 0
-    ):
-        solver_1 = solver(
-            ulds=best_split_uld_splits[0],
-            packages=partition_1,
-        )
-        await solver_1.solve(only_check_fits=False)
-        await solver_1.get_fit()
-
-    if (
-        best_split_uld_splits[1] is not None
-        and len(partition_2) > 0
-        and len(best_split_uld_splits[1]) > 0
-    ):
-        solver_2 = solver(
-            ulds=best_split_uld_splits[1],
-            packages=partition_2,
-        )
-        await solver_2.solve(only_check_fits=False)
-        await solver_2.get_fit()
-
-    return (calculate_cost(packages, k_cost), packages, ulds)
