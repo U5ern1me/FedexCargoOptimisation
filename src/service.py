d07fa8ee0f3580c12a752a914c6e07cf1bba8b59
Parth Patil <psp90804@gmail.com>
2024-12-07 06:39:01 +0530

    fixed api

--- src/service.py
+++ src/service.py
@@ -3,14 +3,16 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from fastapi import FastAPI, UploadFile, File, Query, Body
+from fastapi import FastAPI, UploadFile, File, Query, Body, HTTPException, status
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 import asyncio
 import shutil
-
+import time
 from strategies import strategies
-from utils.io import read_ulds, read_packages, load_config
+from utils.io import read_ulds, read_packages, load_config, write_output
+from concurrent.futures import ThreadPoolExecutor
 
 config = load_config(os.path.join(os.path.dirname(__file__), "service.config"))
 
@@ -30,6 +32,15 @@ class APIService:
             os.makedirs(self.output_folder)
 
         self.solutions = {}  # Dictionary to store solver instances
+        self.event_loop = asyncio.get_event_loop()
+
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
         # Register routes
         self.app.post("/filepack")(self.upload_files)
@@ -47,6 +58,40 @@ class APIService:
                 shutil.rmtree(os.path.join(self.input_folder, request_id))
                 del self.solutions[request_id]
 
+    async def code_runner(self, request_id: str):
+        """
+        Code to run the solver.
+        """
+        try:
+            strategy = self.solutions[request_id]["instance"]
+            if not await strategy.post_process():
+                await strategy.solve()
+                await strategy.gravity_stabilization()
+
+                if await strategy.validate():
+                    self.solutions[request_id]["status"] = 1
+                    write_output(
+                        *await strategy.get_outputs(),
+                        self.solutions[request_id]["output_path"],
+                    )
+                else:
+                    self.solutions[request_id]["status"] = 2
+                    self.solutions[request_id]["error"] = strategy.error
+        except Exception as e:
+            self.solutions[request_id]["error"] = str(e)
+            self.solutions[request_id]["status"] = 2
+
+        self.solutions[request_id]["time_end"] = time.time()
+
+    def code_runner_sync(self, request_id: str):
+        asyncio.run(self.code_runner(request_id))
+
+    async def code_runner_wrapper(self, request_id: str):
+        executor = ThreadPoolExecutor(max_workers=3)
+        await self.event_loop.run_in_executor(
+            executor, self.code_runner_sync, request_id
+        )
+
     async def upload_files(
         self,
         request_name: str = Body(..., description="Name of the request"),
@@ -55,9 +100,8 @@ class APIService:
         package_file: UploadFile = File(..., description="Package file"),
         strategy: str = Body(
             ...,
-            description=f"The strategy to use. Has values: {config['strategies']}",
+            description=f"The strategy to use. Has values:",
             required=False,
-            default=config["default strategy"],
         ),
     ):
         """
@@ -70,18 +114,20 @@ class APIService:
             output_path = os.path.join(self.output_folder, folder_name)
 
             if request_name == "":
-                return JSONResponse(
-                    content={"error": "Request name cannot be empty"}, status_code=400
+                raise HTTPException(
+                    detail="Request name cannot be empty",
+                    status_code=status.HTTP_400_BAD_REQUEST,
                 )
             elif request_name in self.solutions:
-                return JSONResponse(
-                    content={"error": "Request name already exists"}, status_code=400
+                raise HTTPException(
+                    detail="Request name already exists",
+                    status_code=status.HTTP_400_BAD_REQUEST,
                 )
 
             if k < 0:
                 return JSONResponse(
-                    content={"error": "K must be greater than or equal to 0"},
-                    status_code=400,
+                    detail="K must be greater than or equal to 0",
+                    status_code=status.HTTP_400_BAD_REQUEST,
                 )
 
             if DEBUG:
@@ -104,37 +150,52 @@ class APIService:
             with open(file_paths["package"], "wb") as f:
                 shutil.copyfileobj(package_file.file, f)
 
-            ulds = read_ulds(file_paths["uld"])
-            packages = read_packages(file_paths["package"])
+            ulds = read_ulds(folder_path)
+            packages = read_packages(folder_path)
 
             if len(ulds) == 0:
-                return JSONResponse(
-                    content={"error": "Uld file is empty"}, status_code=400
+                raise HTTPException(
+                    detail="Uld file is empty",
+                    status_code=status.HTTP_400_BAD_REQUEST,
                 )
             if len(packages) == 0:
-                return JSONResponse(
-                    content={"error": "Package file is empty"}, status_code=400
+                raise HTTPException(
+                    detail="Package file is empty",
+                    status_code=status.HTTP_400_BAD_REQUEST,
                 )
 
             SelectedStrategy = strategies[strategy]
 
-            self.solutions[request_name] = SelectedStrategy(
-                ulds=ulds, packages=packages, k_cost=k
-            )
+            self.solutions[request_name] = {
+                "status": 0,  # 0 - solving, 1 - done, 2 - error
+                "time_start": time.time(),
+                "time_end": None,
+                "error": None,
+                "instance": SelectedStrategy(
+                    ulds=ulds,
+                    packages=packages,
+                    k_cost=k,
+                    output_path=self.output_folder,
+                ),
+                "output_path": output_path,
+            }
 
             # Create an async task for the solver using thread pool
-            asyncio.create_task(self.solutions[request_name].run())
+            asyncio.create_task(self.code_runner_wrapper(request_name))
 
             return JSONResponse(
                 content={
                     "message": "Files uploaded successfully",
                     "request_id": request_name,
                 },
-                status_code=200,
+                status_code=status.HTTP_200_OK,
             )
-
+        except HTTPException as e:
+            raise e
         except Exception as e:
-            return JSONResponse(content={"error": str(e)}, status_code=500)
+            raise HTTPException(
+                detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
     async def check_status(self, request_id: str):
         """
@@ -145,25 +206,42 @@ class APIService:
                 print(f"Checking status for {request_id}")
 
             if request_id not in self.solutions:
-                return JSONResponse(
-                    content={"error": "Request ID not found"}, status_code=404
+                raise HTTPException(
+                    detail="Request ID not found",
+                    status_code=status.HTTP_400_BAD_REQUEST,
                 )
 
-            has_completed = self.solutions[request_id].solution_found
-            time_end = (
-                self.solutions[request_id].time_end if has_completed else datetime.now()
-            )
+            run_status = self.solutions[request_id]["status"]
 
-            return JSONResponse(
-                content={
-                    "status": ("completed" if has_completed else "solving"),
+            if run_status == 0:
+                content = {
+                    "status": "solving",
                     "time_taken": round(
-                        time_end - self.solutions[request_id].time_start, 2
+                        time.time() - self.solutions[request_id]["time_start"], 2
                     ),
                 }
-            )
+            elif run_status == 1:
+                content = {
+                    "status": "completed",
+                    "time_taken": round(
+                        self.solutions[request_id]["time_end"]
+                        - self.solutions[request_id]["time_start"],
+                        2,
+                    ),
+                }
+            else:
+                raise HTTPException(
+                    detail=self.solutions[request_id]["error"],
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
+
+            return JSONResponse(content=content)
+        except HTTPException as e:
+            raise e
         except Exception as e:
-            return JSONResponse(content={"error": str(e)}, status_code=500)
+            raise HTTPException(
+                detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
     async def get_solution(self, request_id: str):
         """
@@ -174,24 +252,37 @@ class APIService:
                 print(f"Getting solution for {request_id}")
 
             if request_id not in self.solutions:
-                return JSONResponse(
-                    content={"error": "Request ID not found"}, status_code=404
+                raise HTTPException(
+                    detail="Request ID not found",
+                    status_code=status.HTTP_400_BAD_REQUEST,
                 )
 
-            if not self.solutions[request_id].solution_found:
-                return JSONResponse(
-                    content={"error": "The solution is not yet available"},
-                    status_code=404,
+            if self.solutions[request_id]["status"] == 0:
+                raise HTTPException(
+                    detail="The solution is not yet available",
+                    status_code=status.HTTP_404_NOT_FOUND,
+                )
+
+            if self.solutions[request_id]["status"] == 2:
+                raise HTTPException(
+                    detail=self.solutions[request_id]["error"],
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 )
 
             return JSONResponse(
                 content={
                     "request_id": request_id,
-                    "solution": await self.solutions[request_id].get_solution_json(),
+                    "solution": await self.solutions[request_id][
+                        "instance"
+                    ].get_solution_json(),
                 }
             )
+        except HTTPException as e:
+            raise e
         except Exception as e:
-            return JSONResponse(content={"error": str(e)}, status_code=500)
+            raise HTTPException(
+                detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
 
 # Initialize the service
