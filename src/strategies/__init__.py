0e5e5eea277a6fce1f5f97d4c864dfa869a2f5cd
Parth Patil <psp90804@gmail.com>
2024-12-07 19:06:36 +0530

    fix requirements.txt

--- src/strategies/__init__.py
+++ src/strategies/__init__.py
@@ -1,14 +1,14 @@
-# from .gurobi_strategy import GurobiStrategy
-# from .drl_strategy import DRLStrategy
-# from .hexaly_strategy import HexalyStrategy
+from .gurobi_strategy import GurobiStrategy
+from .drl_strategy import DRLStrategy
+from .hexaly_strategy import HexalyStrategy
 from .brkga_strategy import BRKGAStrategy
 from .genetic_algorithm_strategy import GeneticAlgorithmStrategy
 from .greedy_heuristic_strategy import GreedyHeuristicStrategy
 
 strategies = {
-    # "drl": DRLStrategy,
-    # "gurobi": GurobiStrategy,
-    # "hexaly": HexalyStrategy,
+    "drl": DRLStrategy,
+    "gurobi": GurobiStrategy,
+    "hexaly": HexalyStrategy,
     "brkga": BRKGAStrategy,
     "genetic_algorithm": GeneticAlgorithmStrategy,
     "greedy_heuristic": GreedyHeuristicStrategy,
